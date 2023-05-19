6a816a7b414b1c018af9950daa6177ce6920eff6
malic <yvesmalick.mao@edu.ece.fr>
2023-05-19 17:56:28 +0200

    p

--- script.js
+++ script.js
@@ -4,15 +4,23 @@ const defaultTodo = [
 ]
 
 const displayTodos = (listTodo) => {
-
+console.log(listTodo)
 }
 
 
 // #1 selectionner le bouton avec l'id #add, le container #container-list, le input #new
 const btn = document.querySelector('#btnadd')
-const 
+const input = document.querySelector('#new')
+const container = document.querySelector('#container-list')
 // #2 Afficher les todos dans le container avec une fonction displayTodos
+displayTodos.forEach(listTodo => {
 
+    container.innerHTML += ` <div class="border bg-slate-800 p-8 rounded-xl mt-4">
+                           ${listTodo.done}
+                           ${listTodo.name}
+                            </div>
+    `
+});
 
 // #3 Ajouter un event listener sur le bouton pour ajouter un todo
 
